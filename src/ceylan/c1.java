7fb3652e1e45f76fbf30f38bec3f68142ed59522
xezall <ceylanxezalkaya@gmail.com>
2021-06-30 23:09:33 -0400

    "dem"

---
+++ src/ceylan/c1.java
@@ -0,0 +1,14 @@
+package ceylan;
+
+public class c1 {
+    public static void main(String[] args) {
+        System.out.println("We don't need no education");
+        //echo "# Team4java" >> README.md
+        //git init
+        //git add README.md
+        //git commit -m "first commit"
+        //git branch -M main
+        //git remote add origin https://github.com/xezall/Team4java.git
+        //git push -u origin main
+    }
+}
